c5d482a1bc0ab257504f5b47240f2d50139df179
jacksonjude <jacksonjude@mac.com>
2018-08-26 17:26:45 -0700

    Fixed saving and loading of "A" teachers

--- index.js
+++ index.js
@@ -1262,6 +1262,15 @@ function saveSession(id)
 {
     console.log("Saving Data...")
 
+    var checkedSelectedTeachers = selectedTeachers
+    for (teacherArrayNum in checkedSelectedTeachers)
+    {
+        for (teacherNum in checkedSelectedTeachers[teacherArrayNum])
+        {
+            checkedSelectedTeachers[teacherArrayNum][teacherNum] = checkedSelectedTeachers[teacherArrayNum][teacherNum].replace(new RegExp("\"", 'g'), "\\\"")
+        }
+    }
+
     var dataToSave = {"command":"save", "id":id, "coursesJSON":JSON.stringify(selectedCourseCodes), "teachersJSON":JSON.stringify(selectedTeachers), "offBlocksJSON":JSON.stringify(selectedOffBlocks), "filtersJSON":JSON.stringify(filters), "favoriteSchedulesJSON":JSON.stringify(favoriteSchedules)}
     console.log(dataToSave)
 
